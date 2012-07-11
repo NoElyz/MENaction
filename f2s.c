b60499d11d4328b49361a2776f10da8f178ce219
Itsurin Syu <itsurin.syu@gmail.com>
2012-07-11 19:56:30 +0900

    add lines to f2s.c, but still some errors need to be check.

--- f2s.c
+++ f2s.c
@@ -44,5 +44,90 @@ int main (int argc, char *argv[])
 		return -1;
 	}
 	avcodec_open(aCodecCtx,aCodec);
+
+	// The queues part
+	typedef struct PacketQueue{
+		AVPacketList *first_pkt, *last_pkt;
+		int nb_packets;// wrong Bint in the pdf--use int instead upon the web version
+		int size;
+		SDL_mutex *mutex;
+		SDL_cond *cond;
+	}PacketQueue;
+
+	// Initialize the queue
+	void packet_queue_init(PacketQueue *q) {
+		memset (q, 0, sizeof(PacketQueue));
+		q->mutex = SDL_CreateMutex();
+		q->cond = SDL_CreateCond();
+	}
+	
+	// Put stuff into the queue
+	int packet_queue_put(PacketQueue  *q,AVPacket *pkt){
+		AVPacketList *pkt1;
+		if(av_dup_packet(pkt)<0){
+			return -1;
+		}
+		pkt1 = av_malloc(sizeof(AVPacketList));
+
+		if (!pkt1)
+			return -1;
+		pkt1->pkt = *pkt;
+		pkt1->next = NULL;
+
+		SDL_LockMutex(q->mutex);
+
+		if (!q->last_pkt)
+			q->first_pkt = pkt1;
+		else
+			q->last_pkt->next = pkt1;
+
+		q->last_pkt =pkt1;
+		q->nb_packets++;
+		q->size += pkt1->pkt.size;
+		SDL_CondSignal(q->cond);
+
+		SDL_UnlockMutex(q->mutex);
+		return 0;
+	}
+
+	int quit = 0;
+
+	static int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block){ // invalid storage class for function packet_queue_get appears
+		AVPacketList *pkt1;
+		int ret;
+
+		SDL_LockMutex(q->mutex);
+
+		for(;;){
+
+			if(quit){
+				ret = -1;
+				break;
+			}
+
+		pkt1 = q->first_pkt;
+
+		if (pkt1) {
+			q->first_pkt = pkt1->next;
+
+			if (!q->first_pkt)
+				q->last_pkt = NULL;
+
+			q->nb_packets--;
+			q->size -= pkt1->pkt.size;
+			*pkt = pkt1->pkt;
+			av_free(pkt1);
+			ret =1;
+			break;
+			} else if (!block) {
+				ret = 0;
+				break;
+			} else {
+				SDL_CondWait(q->cond, q->mutex);
+			}
+		}
+	SDL_UnlockMutex(q->mutex);
+	return ret;
+	}
 }
 
